4c1fd1ec8d3ec93504e81dbaed5c7269e3bcae61
Kaio <kaiocesarnp@gmail.com>
2022-09-06 13:52:55 -0300

    iniciando o projeto

---
+++ jscript.js
@@ -0,0 +1,19 @@
+var btn = document.querySelector("#back-to-top");
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+});
+
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+  
