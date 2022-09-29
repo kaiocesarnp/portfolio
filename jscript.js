95947ccc092ac7dcc9cd86c48a5b5ea7f80fca51
kaiocesarnp <kaiocesarnp@gmail.com>
2022-09-29 01:29:11 -0300

    atualizando portfolio

--- jscript.js
+++ jscript.js
@@ -1,7 +1,7 @@
-var btn = document.querySelector("#back-to-top");
-btn.addEventListener("click", function() {
-    window.scrollTo(0, 0);
-});
+// var btn = document.querySelector("#back-to-top");
+// btn.addEventListener("click", function() {
+//     window.scrollTo(0, 0);
+// });
 
 var swiper = new Swiper(".mySwiper", {
     cssMode: true,
